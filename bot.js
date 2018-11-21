58e147d9d22d7b0ad3f9da0c5cc413ba8eb20719
mohamedpower <44884883+mohamedpower@users.noreply.github.com>
2018-11-22 00:24:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,31 @@ client.on('message', function(message) {
 
 });
 
+const invites = {};
+
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  wait(1000);
+
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const logChannel = member.guild.channels.find(channel => channel.name === "galaxy");
+    logChannel.send(`${member} Invited by: <@${inviter.id}>`);
+  });
+});
+
 client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","inviters"));
     });
