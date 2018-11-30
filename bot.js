7988140bba627d8ceb51fc6378383ce5114cbefe
mohamedpower <44884883+mohamedpower@users.noreply.github.com>
2018-11-30 02:13:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,64 @@ client.user.setGame(`Galaxy`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
+client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
+
+  member.guild.fetchInvites().then(guildInvites => {
+
+    const ei = invites[member.guild.id];
+
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+
+    const inviter = client.users.get(invite.inviter.id);
+
+    const stewart = member.guild.channels.find("name", "welcome");
+
+     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+
+  });
+
+})
+
+client.on('message', function(msg) {
+
+         var prefix = "+"
+
+    if(msg.content.startsWith (prefix  + 'server')) {
+
+      let embed = new Discord.RichEmbed()
+
+      .setColor('RANDOM')
+
+      .setThumbnail(msg.guild.iconURL)
+
+      .setTitle(`Showing Details Of  **${msg.guild.name}*`)
+
+      .addField('🌐** نوع السيرفر**',`[** __${msg.guild.region}__ **]`,true)
+
+      .addField('🏅** __الرتب__**',`[** __${msg.guild.roles.size}__ **]`,true)
+
+      .addField('🔴**__ عدد الاعضاء__**',`[** __${msg.guild.memberCount}__ **]`,true)
+
+      .addField('🔵**__ عدد الاعضاء الاونلاين__**',`[** __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
+
+      .addField('📝**__ الرومات الكتابية__**',`[** __${msg.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
+
+      .addField('🎤**__ رومات الصوت__**',`[** __${msg.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
+
+      .addField('👑**__ الأونـر__**',`**${msg.guild.owner}**`,true)
+
+      .addField('🆔**__ ايدي السيرفر__**',`**${msg.guild.id}**`,true)
+
+      .addField('📅**__ تم عمل السيرفر في__**',msg.guild.createdAt.toLocaleString())
+
+      msg.channel.send({embed:embed});
+
+    }
+
+  });
+
 client.on('guildMemberAdd', msg => { 
     var embed = new Discord.RichEmbed()
     .setAuthor(msg.user.username, msg.user.avatarURL)
